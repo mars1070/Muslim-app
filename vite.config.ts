a7e13ae29be11a911b4d273b87e0f13607b29637
mars1070 <your.email@example.com>
2025-11-07 14:48:05 +0100

    Configuration du déploiement GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig(({ mode }) => {
     const isProduction = mode === 'production';
     
     return {
-      base: '/',
+      base: '/Muslim-app/',
       server: {
         port: 3000,
         host: '0.0.0.0',
